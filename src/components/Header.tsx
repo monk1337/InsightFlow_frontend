5a33f48b01f34cf149aef58ade3b9475fe223c28
BLooDBRothER <computertechnology446@gmail.com>
2023-12-28 14:02:14 +0530

    Add login and signup UI

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <>
+      <div className='p-2 h-[60px] flex items-center justify-between'>
+        <h1>Dataset Collection</h1>
+      </div>
+    </>
+  )
+}
+
+export default Header
