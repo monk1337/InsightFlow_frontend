8169f8da8bf4125de71fec1cf17b466ca906c93b
BLooDBRothER <computertechnology446@gmail.com>
2024-01-13 20:34:13 +0530

    Refactor layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,18 @@
+import { useUserContext } from "@/context/userContext"
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { isUserLogged} = useUserContext();
+
+  if(!isUserLogged)
+    return <></>
+
   return (
     <>
       <div className='p-2 h-[60px] flex items-center justify-between'>
-        <h1>Dataset Collection</h1>
+        <Link to="/">
+          <h1>Voicecord</h1>
+        </Link>
       </div>
     </>
   )
--- src/pages/home/AuthHome.tsx
+++ src/pages/home/AuthHome.tsx
@@ -1,29 +1,28 @@
 import { Button, Typography } from '@mui/material'
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 const AuthHome = () => {
-  return (
-    <div className='h-[100vh] flex items-center justify-center'>
-        <div className='card'>
-            <Typography variant="h2" className='!mb-20'>
-                Voicecord
-            </Typography>
-            <div className='flex flex-col gap-4 items-center justify-center'>
-                <Link to="/login?role=recorder">
-                    <Button variant='contained'>Login as Recorder</Button>
-                </Link>
-                <Link to="/login?role=reviewer">
-                    <Button variant='outlined'>Login as Reviewer</Button>
-                </Link>
-                <div className='text-center mt-4'>
-                    <div className='dark:text-dark-l-gray'>Don't have a Project</div>
-                    <Link to="/new" className='dark:text-dark-accent underline'>Create Project</Link>
+    return (
+        <div className='full-screen'>
+            <div className='card'>
+                <Typography variant="h3" className='!mb-14'>
+                    Voicecord
+                </Typography>
+                <div className='flex flex-col gap-4 items-center justify-center'>
+                    <Link to="/login?role=recorder">
+                        <Button variant='contained'>Login as Recorder</Button>
+                    </Link>
+                    <Link to="/login?role=reviewer">
+                        <Button variant='outlined'>Login as Reviewer</Button>
+                    </Link>
+                    <div className='text-center mt-4'>
+                        <div className='dark:text-dark-l-gray'>Don't have a Project</div>
+                        <Link to="/new" className='dark:text-dark-accent underline'>Create Project</Link>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default AuthHome
